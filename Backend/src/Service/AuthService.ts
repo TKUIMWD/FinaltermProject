dde40bb035be3cb7fee37eaefb6969c00489b0d3
powen <powen0966091716@gmail.com>
2024-12-21 10:38:12 +0800

    Merge from powenn , add auth/logout (#6)

--- Backend/src/Service/AuthService.ts
+++ Backend/src/Service/AuthService.ts
@@ -5,13 +5,13 @@ import { adminsModel } from "../orm/schemas/adminsSchemas";
 import { resp } from "../utils/resp";
 import { DBResp } from "../interfaces/DBResp";
 import { Document } from "mongoose";
-import { generateToken } from "../utils/token";
+import { generateToken, verifyToken } from "../utils/token";
 import { AuthResponse } from "../interfaces/AuthResponse";
 import { logger } from "../middlewares/log";
 
 export class AuthService extends Service {
 
-    public async register(data: { username: string, password: string, phone_num: string , email:string }): Promise<resp<DBResp<Document> | undefined>> {
+    public async register(data: { username: string, password: string, phone_num: string, email: string }): Promise<resp<DBResp<Document> | undefined>> {
         const resp: resp<DBResp<Document> | undefined> = {
             code: 200,
             message: "",
@@ -34,7 +34,7 @@ export class AuthService extends Service {
             }
 
             const password_hash = await bcrypt.hash(password, 10);
-            const res = new usersModel({ username, password_hash, phone_num , email });
+            const res = new usersModel({ username, password_hash, phone_num, email });
             await res.save();
             logger.info(`User ${username} registered`);
             resp.message = "Register succeed";
@@ -103,4 +103,50 @@ export class AuthService extends Service {
         }
         return resp;
     }
+
+
+    // logout with jwt token
+    public async logout(data: { token: string }): Promise<resp<undefined>> {
+        const resp: resp<undefined> = {
+            code: 200,
+            message: "",
+            body: undefined
+        };
+        try {
+            const decoded = verifyToken(data.token);
+            if (!decoded) {
+                resp.code = 400;
+                resp.message = "Invalid token";
+                return resp;
+            }
+            const { _id, role } = decoded as { _id: string, role: string };
+            let username = '';
+
+            if (role === 'user') {
+                const user = await usersModel.findOne({ _id });
+                if (user) {
+                    username = user.username;
+                }
+            } else if (role === 'admin') {
+                const admin = await adminsModel.findOne({ _id });
+                if (admin) {
+                    username = admin.username;
+                }
+            }
+
+            if (username) {
+                logger.info(`${role.charAt(0).toUpperCase() + role.slice(1)} ${username} logged out`);
+            } else {
+                logger.info(`User with ID ${_id} and role ${role} logged out`);
+            }
+
+            resp.message = "Logout succeed";
+        } catch (error) {
+            resp.code = 500;
+            resp.message = "Server error";
+            console.error("Error in logout:", error);
+        }
+        return resp;
+    }
+
 }
