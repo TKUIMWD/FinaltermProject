f2d4cb4936dcb046f0ac306792e5a0330bb8fe80
powen <powen0966091716@gmail.com>
2025-01-01 12:49:45 +0800

    update loginmdoal

--- Frontend/src/component/LoginModal.tsx
+++ Frontend/src/component/LoginModal.tsx
@@ -1,7 +1,10 @@
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import { Form, Button, Container, Row, Col, Toast } from 'react-bootstrap';
 import { BaseImgPath } from '../data/BaseImgPath';
 import '../style/LoginModal.css';
 import { Person, Lock } from 'react-bootstrap-icons';
+import { asyncPost } from '../utils/fetch';
+import { auth_api } from '../enum/api';
 
 interface LoginModalProps {
     handleShowRegister: () => void;
@@ -10,6 +13,38 @@ interface LoginModalProps {
 export default function LoginModal({ handleShowRegister }: LoginModalProps) {
     const DWRP_logo = BaseImgPath + 'DWRP.jpg';
     const iconSize = 65;
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showToast, setShowToast] = useState(false);
+    const [toastMessage, setToastMessage] = useState('');
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const response = await asyncPost(auth_api.login, {
+            username,
+            password,
+        });
+
+        if (response.code === 200) {
+            localStorage.setItem('token', response.body.token);
+            const token = localStorage.getItem('token');
+            if (token) {
+                setToastMessage('Login succeed');
+                setShowToast(true);
+                setTimeout(() => {
+                    window.location.reload();
+                }, 1000);
+            } else {
+                setToastMessage('Failed to save token');
+                setShowToast(true);
+            }
+        } else {
+            setToastMessage(response.message);
+            setShowToast(true);
+        }
+    };
 
     return (
         <Container className="login-modal-container modal-container">
@@ -24,14 +59,19 @@ export default function LoginModal({ handleShowRegister }: LoginModalProps) {
                     />
                 </Col>
             </Row>
-            <Form>
+            <Form onSubmit={handleLogin}>
                 <Form.Group controlId="formBasicUsername" className="position-relative">
                     <Form.Label>使用者名稱</Form.Label>
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <span className="input-group-text"><Person /></span>
                         </div>
-                        <Form.Control type="text" placeholder="輸入使用者名稱" />
+                        <Form.Control
+                            type="text"
+                            placeholder="輸入使用者名稱"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
                 </Form.Group>
 
@@ -41,7 +81,12 @@ export default function LoginModal({ handleShowRegister }: LoginModalProps) {
                         <div className="input-group-prepend">
                             <span className="input-group-text"><Lock /></span>
                         </div>
-                        <Form.Control type="password" placeholder="輸入密碼" />
+                        <Form.Control
+                            type="password"
+                            placeholder="輸入密碼"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                 </Form.Group>
 
@@ -53,6 +98,16 @@ export default function LoginModal({ handleShowRegister }: LoginModalProps) {
                     沒有帳號？
                 </Button>
             </Form>
+
+            <Toast
+                onClose={() => setShowToast(false)}
+                show={showToast}
+                delay={1000}
+                autohide
+                className="position-fixed top-0 start-50 translate-middle-x mt-3"
+            >
+                <Toast.Body>{toastMessage}</Toast.Body>
+            </Toast>
         </Container>
     );
 }
